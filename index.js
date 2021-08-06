2d6133983b0cffe8728de1204c094d3ea115c9f5
Shafin <shafinhasnat97@gmail.com>
2021-08-06 22:56:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 4000;
+
+var todo = [];
+
+app.get('/todo', (req, res) => {
+    res.send(todo);
+});
+
+app.post('/todo', (req, res) => {
+    const { title, description, done } = req.body;
+    const id = randomBytes(4).toString('hex');
+    const time = new Date();
+    const data = {
+        id,
+        done,
+        title,
+        description,
+        time
+    }
+    todo.push(data);
+    res.status(201).send(todo[id])
+});
+
+app.delete('/todo/:id', (req, res) => {
+    const id = req.params.id;
+    todo = todo.filter((item) => item.id !== id)
+    res.send();
+});
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
